5e0691a5a738c92a3e94f01d63d0e939ee774aca
devjoliveira <macpro@MacBook-Pro-de-MacPro.local>
2025-05-21 01:45:01 -0300

    Create project

---
+++ src/test/java/com/devjoliveira/game_list/GameListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devjoliveira.game_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
